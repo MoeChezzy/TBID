03056ec303c3c6c367db281f463623d961fa2820
Chezzy <chezrye@gmail.com>
2016-01-11 19:15:10 -0500

    Added mutex block in main Program.cs
    This is to prevent more than one instance of TBID from running at the
    same time.

--- TBID/Program.cs
+++ TBID/Program.cs
@@ -4,11 +4,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Threading;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
 namespace TBID
 {
     static class Program
     {
         public const string Name = "TBID";
+
+        private static string GUID = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
 
         /// <summary>
         /// The main entry point for the application.
@@ -18,8 +26,53 @@ namespace TBID
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            
-            Application.Run(new MainForm());
+
+            // Initialize variables for mutex.
+            string MutexID = string.Format("Global\\{{{0}}}", GUID);
+            bool CreatedNewMutex;
+            MutexAccessRule AllowEveryoneRule = new MutexAccessRule(new SecurityIdentifier(WellKnownSidType.WorldSid, null), MutexRights.FullControl, AccessControlType.Allow);
+            MutexSecurity SecuritySettings = new MutexSecurity();
+            SecuritySettings.AddAccessRule(AllowEveryoneRule);
+
+            using (Mutex mutex = new Mutex(false, MutexID, out CreatedNewMutex, SecuritySettings))
+            {
+                bool HandleAcquired = false;
+                try
+                {
+                    try
+                    {
+                        HandleAcquired = mutex.WaitOne(2000, false);
+                        if (!HandleAcquired)
+                        {
+                            MessageBox.Show("There is an instance of " + Name + " already currently running.", Name + " already open.", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            Close();
+                        }
+                    }
+                    catch (AbandonedMutexException)
+                    {
+                        // The mutex was abandoned in another process. It will still get acquired.
+                        HandleAcquired = true;
+                    }
+
+                    // Begin program.
+                    Application.Run(new MainForm());
+                    // End program.
+                }
+                finally
+                {
+                    // Release the mutex if it was acquired.
+                    if (HandleAcquired)
+                        mutex.ReleaseMutex();
+                }
+            }
+        }
+
+        public static void Close()
+        {
+            if (Application.MessageLoop)
+                Application.Exit();
+            else
+                Environment.Exit(0);
         }
     }
 }
