424fc9920071b946baeb7caf9949deaa1fb3c7a3
Chezzy <chezrye@gmail.com>
2016-03-19 17:48:04 -0400

    Revert
    I don't know. Even.

--- TBID/Program.cs
+++ TBID/Program.cs
@@ -1,29 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Security.AccessControl;
 using System.Security.Principal;
-using System.Threading;
-using System.Windows.Forms;
 
 namespace TBID
 {
-    internal static class Program
+    static class Program
     {
         public const string Name = "TBID";
         public const string Author = "Chezzy";
 
-<<<<<<< HEAD
         private static string Guid = ((GuidAttribute)Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(GuidAttribute), false).GetValue(0)).Value.ToString();
-=======
-        private static readonly string GUID = ((GuidAttribute) Assembly.GetExecutingAssembly().GetCustomAttributes(typeof (GuidAttribute), false).GetValue(0)).Value;
->>>>>>> origin/master
 
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        private static void Main()
+        static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
@@ -35,7 +35,6 @@ namespace TBID
             MutexSecurity SecuritySettings = new MutexSecurity();
             SecuritySettings.AddAccessRule(AllowEveryoneRule);
 
-<<<<<<< HEAD
             using (Mutex Mutex = new Mutex(false, MutexId, out CreatedNewMutex, SecuritySettings))
             {
                 bool HandleAcquired = false;
@@ -46,19 +45,12 @@ namespace TBID
                         HandleAcquired = Mutex.WaitOne(2000, false);
                         if (!HandleAcquired)
                         {
-=======
-            using (Mutex mutex = new Mutex(false, MutexID, out CreatedNewMutex, SecuritySettings)) {
-                bool HandleAcquired = false;
-                try {
-                    try {
-                        HandleAcquired = mutex.WaitOne(2000, false);
-                        if (!HandleAcquired) {
->>>>>>> origin/master
                             MessageBox.Show("There is an instance of " + Name + " already currently running.", Name + " already open.", MessageBoxButtons.OK, MessageBoxIcon.Information);
                             Close();
                         }
                     }
-                    catch (AbandonedMutexException) {
+                    catch (AbandonedMutexException)
+                    {
                         // The mutex was abandoned in another process, so in this case it will still get acquired.
                         HandleAcquired = true;
                     }
@@ -67,7 +59,8 @@ namespace TBID
                     Application.Run(new MainForm());
                     // End program.
                 }
-                finally {
+                finally
+                {
                     // Release the mutex if it was acquired.
                     if (HandleAcquired)
                         Mutex.ReleaseMutex();
@@ -77,8 +70,10 @@ namespace TBID
 
         public static void Close()
         {
-            if (Application.MessageLoop) Application.Exit();
-            else Environment.Exit(0);
+            if (Application.MessageLoop)
+                Application.Exit();
+            else
+                Environment.Exit(0);
         }
     }
 }
